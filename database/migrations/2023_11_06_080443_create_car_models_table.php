7e4dbd73e0a3d8a271735469c7ef4eb950f1a938
itsterit <itsterit@gmail.com>
2023-11-06 13:10:15 +0400

    добавить is_admin в модель миграции пользователей и изменить тип данных brand_id и car_brand_id для успешной миграции

---
+++ database/migrations/2023_11_06_080443_create_car_models_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('car_models', function (Blueprint $table) {
+            $table->bigIncrements('model_id');
+            $table->string('model_name');
+            $table->bigInteger('car_brand_id');
+            $table->timestamps();
+            
+            $table->foreign('car_brand_id')->references('brand_id')->on('brands')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('car_models');
+    }
+}
