641ecda211ee37d9b6d090425d3404b7d15186bc
itsterit <itsterit@gmail.com>
2023-11-09 22:06:32 +0400

    проблема именного индекса массива

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,7 +39,7 @@
         @if ( Auth::user()->is_admin )
             <form class="d-flex flex-row justify-content-end ">
                 <div class="mr-2">
-                    <input type="text" class="form-control col-12" id="inputPassword2" placeholder="Добавить ">
+                    <input type="text" class="form-control col-12" id="inputPassword2" placeholder="Добавить новый тип">
                 </div>
                 <div class="">
                     <button class="btn btn-outline-primary" type="button">Добавить</button>
@@ -49,45 +49,58 @@
     </div>
 
     <div class="mb-4">
-        <select class="form-control mb-2">
-            <option selected>Тип двигателя(неопределен)</option>
-            @if(count($EngineTypes))
-                @foreach($EngineTypes as $engin_type)
-                    <option>{{ $engin_type->engine_type_name }}</option>
-                @endforeach
+        <form method="post" action="">
+            <select class="form-control mb-2" name="EngineTypes">
+                <option selected>Тип двигателя(неопределен)</option>
+                @if(count($EngineTypes))
+                    @foreach($EngineTypes as $engin_type)
+                        <option>{{ $engin_type->engine_type_name }}</option>
+                    @endforeach
+                @endif
+            </select>
+            @if ( Auth::user()->is_admin )
+                <form class="d-flex flex-row justify-content-end ">
+                    <div class="mr-2">
+                        <input type="text" class="form-control col-12" id="inputPassword2" placeholder="Добавить ">
+                    </div>
+                    <div class="">
+                        <button class="btn btn-outline-primary" type="button">Добавить</button>
+                    </div>
+                </form>
             @endif
-        </select>
-        @if ( Auth::user()->is_admin )
-            <form class="d-flex flex-row justify-content-end ">
-                <div class="mr-2">
-                    <input type="text" class="form-control col-12" id="inputPassword2" placeholder="Добавить ">
-                </div>
-                <div class="">
-                    <button class="btn btn-outline-primary" type="button">Добавить</button>
-                </div>
-            </form>
-        @endif
+            <INPUT TYPE="submit" name="Отправить" />
+        </form>
     </div>
 
     <div class="mb-4">
-        <select class="form-control mb-2">
-            <option selected>Тип кузова(неопределен)</option>
-            @if(count($BodyTypes))
-                @foreach($BodyTypes as $body_type)
-                    <option>{{ $body_type->body_type_name }}</option>
-                @endforeach
+        <form method="get" action="/home" id="BodyTypes">
+            <select class="form-control mb-2" name="BodyTypes" onchange="document.getElementById('BodyTypes').submit()">
+                @if( !isset($_GET['BodyTypes']))
+                    <option selected>Тип кузова(неопределен)</option>
+                @else
+                    <option >Тип кузова(неопределен)</option>
+                @endif
+                @if(count($BodyTypes))
+                    @foreach($BodyTypes as $body_type)
+                        @if( $_GET['BodyTypes'] ==  $body_type->body_type_id)
+                            <option selected value="{{ $body_type->body_type_id }}">{{ $body_type->body_type_name }}</option>
+                        @else
+                            <option value="{{ $body_type->body_type_id }}">{{ $body_type->body_type_name }}</option>
+                        @endif
+                    @endforeach
+                @endif
+            </select>
+            @if ( Auth::user()->is_admin )
+                <form class="d-flex flex-row justify-content-end ">
+                    <div class="mr-2">
+                        <input type="text" class="form-control col-12" id="inputPassword2" placeholder="Добавить ">
+                    </div>
+                    <div class="">
+                        <button class="btn btn-outline-primary" type="button">Добавить</button>
+                    </div>
+                </form>
             @endif
-        </select>
-        @if ( Auth::user()->is_admin )
-            <form class="d-flex flex-row justify-content-end ">
-                <div class="mr-2">
-                    <input type="text" class="form-control col-12" id="inputPassword2" placeholder="Добавить ">
-                </div>
-                <div class="">
-                    <button class="btn btn-outline-primary" type="button">Добавить</button>
-                </div>
-            </form>
-        @endif
+        </form>
     </div>
 
     <div class="mb-4">
@@ -110,6 +123,13 @@
             </form>
         @endif
     </div>
+
+    <button class="btn btn-outline-primary" type="submit">Добавить</button>
+
+    <!-- @php
+        echo '<pre>';
+        print_r($_GET);
+    @endphp -->
 
 </div>
 @endsection
