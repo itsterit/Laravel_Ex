4fa86b6d5ea3723085b430b43c34fe7fea4a976b
itsterit <itsterit@gmail.com>
2023-11-10 14:24:12 +0400

    вывод параметров контроллера в представление

--- app/Http/Controllers/StoreDataHandler.php
+++ app/Http/Controllers/StoreDataHandler.php
@@ -17,7 +17,6 @@ class StoreDataHandler extends Controller
         $GET_BodyTypes         = $_GET['BodyTypes']           ?? "";
         $GET_Brands            = $_GET['Brands']              ?? "";
         $GET_Engine            = $_GET['EngineTypes']         ?? "";
-        $GET_Model             = $_GET['Model']               ?? "";
         $GET_IsGoStoreView     = $_GET['IsGoStoreView']       ?? "";
 
         if($GET_IsGoStoreView != 1)
@@ -26,80 +25,14 @@ class StoreDataHandler extends Controller
         }
         else
         {
-            $DB_EngineTypes = "";
-            $DB_BodyTypes   = "";
-            $DB_Brands      = "";
-
             $EngineTypes = enginetypes::all();
             $BodyTypes   = bodytypes::all();
             $Brands      = brands::all();
             $Model       = car_model::all();
 
-            $Cars      = DB::select('select * from car_stores where 1');
-            $CarsParam = DB::select('SELECT `car_model_id`, `car_info`, `was_rented`, `rent_price`, `img_patch`  FROM `car_stores` WHERE 1');
-
             if(($GET_Brands != "" && $GET_Brands != -1)   ||   ($GET_BodyTypes != "" && $GET_BodyTypes != -1)    ||   ($GET_Engine != "" && $GET_Engine != -1))
             {
-                /**
-                 * Вывод авто с "витрины"
-                 */
-                foreach($CarsParam as $Cars_Param)
-                {                  
-                    /**
-                    * бренд авто
-                    */
-                    $CarsModelParam = DB::select('SELECT `car_brand_id`, `car_body_id`, `car_engine_id` FROM `car_models` WHERE model_id = ?', [$Cars_Param->car_model_id]);
-                    foreach($CarsModelParam as $CarsModelParam_list)
-                    {
-                        $DB_Brands = $CarsModelParam_list->car_brand_id;
-                        break;
-                    }
-
-                    /**
-                    * кузов авто
-                    */
-                    $CarsModelParam = DB::select('SELECT `car_brand_id`, `car_body_id`, `car_engine_id` FROM `car_models` WHERE model_id = ?', [$Cars_Param->car_model_id]);
-                    foreach($CarsModelParam as $CarsModelParam_list)
-                    {
-                        $DB_BodyTypes = $CarsModelParam_list->car_body_id;
-                        break;
-                    }
-
-                    /**
-                    * двигатель авто
-                    */
-                    $CarsModelParam = DB::select('SELECT `car_brand_id`, `car_body_id`, `car_engine_id` FROM `car_models` WHERE model_id = ?', [$Cars_Param->car_model_id]);
-                    foreach($CarsModelParam as $CarsModelParam_list)
-                    {
-                        $DB_EngineTypes = $CarsModelParam_list->car_engine_id;
-                        break;
-                    }
-                    
-                    if(    
-                        (($GET_Brands    == $DB_Brands)      || ($GET_Brands    == "" || $GET_Brands    == -1)) &&
-                        (($GET_BodyTypes == $DB_BodyTypes)   || ($GET_BodyTypes == "" || $GET_BodyTypes == -1)) &&
-                        (($GET_Engine    == $DB_EngineTypes) || ($GET_Engine    == "" || $GET_Engine    == -1))
-                    )
-                    {
-                        echo($Cars_Param->car_model_id);
-                        echo("| ");                    
-                        echo($Cars_Param->car_info);
-                        echo(" |");                    
-                        echo($Cars_Param->was_rented);
-                        echo("|");                    
-                        echo($Cars_Param->rent_price);
-                        echo("|");                    
-                        echo($Cars_Param->img_patch);
-                        echo("("); 
-                        
-                        echo($DB_Brands);
-                        echo($DB_BodyTypes);
-                        echo($DB_EngineTypes);
-                        echo(")"); 
-                    }
-                    echo("<br>");
-                }
-
+                return view('OutCars', compact('EngineTypes', 'BodyTypes', 'Brands', 'Model'));
             }
             else
             {
---
+++ resources/views/OutCars.blade.php
@@ -0,0 +1,105 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row  mb-5 mt-5">
+        <div class="col-md-8">
+            <div class="">
+                <div class="text-primary">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <h1>
+                        Hi {{ Auth::user()->name }}, you are logged in!!!
+                    </h1>
+
+                    @if ( Auth::user()->is_admin )
+                        <div class="text-success">
+                            (Активирован статус администратора)
+                        </div>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div> 
+
+    @php
+        $GET_BodyTypes         = $_GET['BodyTypes']           ?? "";
+        $GET_Brands            = $_GET['Brands']              ?? "";
+        $GET_Engine            = $_GET['EngineTypes']         ?? "";
+        $GET_IsGoStoreView     = $_GET['IsGoStoreView']       ?? "";
+        $DB_EngineTypes = "";
+        $DB_BodyTypes   = "";
+        $DB_Brands      = "";       
+        $Cars      = DB::select('select * from car_stores where 1');
+        $CarsParam = DB::select('SELECT `car_model_id`, `car_info`, `was_rented`, `rent_price`, `img_patch`  FROM `car_stores` WHERE 1');
+
+
+        /**
+        * Вывод авто с "витрины"
+        */
+        foreach($CarsParam as $Cars_Param)
+        {
+            /**
+            * бренд авто
+            */
+            $CarsModelParam = DB::select('SELECT `car_brand_id`, `car_body_id`, `car_engine_id` FROM `car_models` WHERE model_id = ?', [$Cars_Param->car_model_id]);
+            foreach($CarsModelParam as $CarsModelParam_list)
+            {
+                $DB_Brands = $CarsModelParam_list->car_brand_id;
+                break;
+            }
+
+            /**
+            * кузов авто
+            */
+            $CarsModelParam = DB::select('SELECT `car_brand_id`, `car_body_id`, `car_engine_id` FROM `car_models` WHERE model_id = ?', [$Cars_Param->car_model_id]);
+            foreach($CarsModelParam as $CarsModelParam_list)
+            {
+                $DB_BodyTypes = $CarsModelParam_list->car_body_id;
+                break;
+            }
+
+            /**
+            * двигатель авто
+            */
+            $CarsModelParam = DB::select('SELECT `car_brand_id`, `car_body_id`, `car_engine_id` FROM `car_models` WHERE model_id = ?', [$Cars_Param->car_model_id]);
+            foreach($CarsModelParam as $CarsModelParam_list)
+            {
+                $DB_EngineTypes = $CarsModelParam_list->car_engine_id;
+                break;
+            }
+
+            if(    
+                (($GET_Brands    == $DB_Brands)      || ($GET_Brands    == "" || $GET_Brands    == -1)) &&
+                (($GET_BodyTypes == $DB_BodyTypes)   || ($GET_BodyTypes == "" || $GET_BodyTypes == -1)) &&
+                (($GET_Engine    == $DB_EngineTypes) || ($GET_Engine    == "" || $GET_Engine    == -1))
+            )
+            {
+                echo($Cars_Param->car_model_id);
+                echo("| ");                    
+                echo($Cars_Param->car_info);
+                echo(" |");                    
+                echo($Cars_Param->was_rented);
+                echo("|");                    
+                echo($Cars_Param->rent_price);
+                echo("|");                    
+                echo($Cars_Param->img_patch);
+                echo("("); 
+                
+                echo($DB_Brands);
+                echo($DB_BodyTypes);
+                echo($DB_EngineTypes);
+                echo(")"); 
+            }
+
+            echo("<br>");
+        }
+    @endphp
+
+</div>
+@endsection
