224ba31402e70bc0a091f84e92ffb01db6862945
itsterit <itsterit@gmail.com>
2023-11-10 14:00:22 +0400

    сортировка

--- app/Http/Controllers/StoreDataHandler.php
+++ app/Http/Controllers/StoreDataHandler.php
@@ -26,13 +26,17 @@ class StoreDataHandler extends Controller
         }
         else
         {
+            $DB_EngineTypes = "";
+            $DB_BodyTypes   = "";
+            $DB_Brands      = "";
+
             $EngineTypes = enginetypes::all();
             $BodyTypes   = bodytypes::all();
             $Brands      = brands::all();
             $Model       = car_model::all();
 
             $Cars      = DB::select('select * from car_stores where 1');
-            $CarsParam = DB::select('SELECT `car_model_id` FROM `car_stores` WHERE 1');
+            $CarsParam = DB::select('SELECT `car_model_id`, `car_info`, `was_rented`, `rent_price`, `img_patch`  FROM `car_stores` WHERE 1');
 
             if(($GET_Brands != "" && $GET_Brands != -1)   ||   ($GET_BodyTypes != "" && $GET_BodyTypes != -1)    ||   ($GET_Engine != "" && $GET_Engine != -1))
             {
@@ -40,60 +44,63 @@ class StoreDataHandler extends Controller
                  * Вывод авто с "витрины"
                  */
                 foreach($CarsParam as $Cars_Param)
-                {
-                    echo($Cars_Param->car_model_id);
-                    echo("(");                    
-                        /**
-                        * бренд авто
-                        */
-                        $CarsModelParam = DB::select('SELECT `car_brand_id`, `car_body_id`, `car_engine_id` FROM `car_models` WHERE model_id = ?', [$Cars_Param->car_model_id]);
-                        foreach($CarsModelParam as $CarsModelParam_list)
-                        {
-                            echo($CarsModelParam_list->car_brand_id);
-                            break;
-                        }
-                    echo(")");
+                {                  
+                    /**
+                    * бренд авто
+                    */
+                    $CarsModelParam = DB::select('SELECT `car_brand_id`, `car_body_id`, `car_engine_id` FROM `car_models` WHERE model_id = ?', [$Cars_Param->car_model_id]);
+                    foreach($CarsModelParam as $CarsModelParam_list)
+                    {
+                        $DB_Brands = $CarsModelParam_list->car_brand_id;
+                        break;
+                    }
 
-                    echo("(");                    
-                        /**
-                        * кузов авто
-                        */
-                        $CarsModelParam = DB::select('SELECT `car_brand_id`, `car_body_id`, `car_engine_id` FROM `car_models` WHERE model_id = ?', [$Cars_Param->car_model_id]);
-                        foreach($CarsModelParam as $CarsModelParam_list)
-                        {
-                            echo($CarsModelParam_list->car_body_id);
-                            break;
-                        }
-                    echo(")");
+                    /**
+                    * кузов авто
+                    */
+                    $CarsModelParam = DB::select('SELECT `car_brand_id`, `car_body_id`, `car_engine_id` FROM `car_models` WHERE model_id = ?', [$Cars_Param->car_model_id]);
+                    foreach($CarsModelParam as $CarsModelParam_list)
+                    {
+                        $DB_BodyTypes = $CarsModelParam_list->car_body_id;
+                        break;
+                    }
 
-                    echo("(");                    
-                        /**
-                        * двигатель авто
-                        */
-                        $CarsModelParam = DB::select('SELECT `car_brand_id`, `car_body_id`, `car_engine_id` FROM `car_models` WHERE model_id = ?', [$Cars_Param->car_model_id]);
-                        foreach($CarsModelParam as $CarsModelParam_list)
-                        {
-                            echo($CarsModelParam_list->car_engine_id);
-                            break;
-                        }
-                    echo(")");
+                    /**
+                    * двигатель авто
+                    */
+                    $CarsModelParam = DB::select('SELECT `car_brand_id`, `car_body_id`, `car_engine_id` FROM `car_models` WHERE model_id = ?', [$Cars_Param->car_model_id]);
+                    foreach($CarsModelParam as $CarsModelParam_list)
+                    {
+                        $DB_EngineTypes = $CarsModelParam_list->car_engine_id;
+                        break;
+                    }
+                    
+                    if(    
+                        (($GET_Brands    == $DB_Brands)      || ($GET_Brands    == "" || $GET_Brands    == -1)) &&
+                        (($GET_BodyTypes == $DB_BodyTypes)   || ($GET_BodyTypes == "" || $GET_BodyTypes == -1)) &&
+                        (($GET_Engine    == $DB_EngineTypes) || ($GET_Engine    == "" || $GET_Engine    == -1))
+                    )
+                    {
+                        echo($Cars_Param->car_model_id);
+                        echo("| ");                    
+                        echo($Cars_Param->car_info);
+                        echo(" |");                    
+                        echo($Cars_Param->was_rented);
+                        echo("|");                    
+                        echo($Cars_Param->rent_price);
+                        echo("|");                    
+                        echo($Cars_Param->img_patch);
+                        echo("("); 
+                        
+                        echo($DB_Brands);
+                        echo($DB_BodyTypes);
+                        echo($DB_EngineTypes);
+                        echo(")"); 
+                    }
 
                     echo("<br>");
                 }
 
-                // dd($CarsParam);
-                // $CarsParam = DB::select('SELECT `car_model_id` FROM `car_stores` WHERE 1');
-                // foreach($Cars as $Cars_card)
-                // {
-                //     if(    
-                //         (($GET_Brands    == $Cars_card->car_id)       || ($GET_Brands    == "" || $GET_Brands    == -1)) &&
-                //         (($GET_BodyTypes == $Cars_card->car_body_id)        || ($GET_BodyTypes == "" || $GET_BodyTypes == -1)) &&
-                //         (($GET_Engine    == $Cars_card->car_engine_id)      || ($GET_Engine    == "" || $GET_Engine    == -1))
-                //     )
-                //     {
-                //         dd($Cars);
-                //     }
-                // }
             }
             else
             {
